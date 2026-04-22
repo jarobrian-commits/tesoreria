e2b2d596d59b56b2cf16d7c5c1f6a6bf45275808
brian <jarobrian@gmail.com>
2026-04-21 21:38:47 -0300

    fix paths api

--- caja/index.php
+++ caja/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../api/auth.php';
+require_once __DIR__ . '/api/auth.php';
 require_once __DIR__ . '/../seguridad.php';
 
 $cliente = $_SESSION['cliente'];
--- tesoreria/index.php
+++ tesoreria/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../api/auth.php';
+require_once __DIR__ . '/api/auth.php';
 require_once __DIR__ . '/../seguridad.php';
 require_once __DIR__ . '/../api/db.php';
 
